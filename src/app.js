bbc74b060036c4180163ded3d672d73469fa7576
Juan Felipe Rios Torres <141080920+JuanFelipeR@users.noreply.github.com>
2025-04-14 20:37:19 +0000

    card-generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  generateCard ();
+};
+function generateCard () {
+  let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  let suits = ['♦', '♥', '♠', '♣'];
+
+  let randomNumber = Math.floor(Math.random() * numbers.length);
+  let randomSuit = Math.floor(Math.random() * suits.length);
+
+  document.querySelector('.top').textContent = suits[randomSuit];
+  document.querySelector('.card-number').textContent = numbers[randomNumber];
+  document.querySelector('.bottom').textContent = suits[randomSuit];
+
+  if (suits[randomSuit] === '♦' || suits[randomSuit] === '♥') {
+      document.querySelector('.top').style.color = 'red';
+      document.querySelector('.bottom').style.color = 'red';
+      document.querySelector('.card-number').style.color = 'red';
+  }
+  else {
+      document.querySelector('.top').style.color = 'black';
+      document.querySelector('.bottom').style.color = 'black';
+      document.querySelector('.card-number').style.color = 'black';
+  }
+
+  setInterval(generateCard, 10000); // temporizador
 };
